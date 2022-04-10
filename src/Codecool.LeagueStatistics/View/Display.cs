1e319faf06c109d21e724858d997dc1b83310965
skris92 <szurcsik92@gmail.com>
2022-04-10 12:32:20 +0200

    DisplayFinalResult method.

--- src/Codecool.LeagueStatistics/View/Display.cs
+++ src/Codecool.LeagueStatistics/View/Display.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 namespace Codecool.LeagueStatistics.View
 {
+    using static Codecool.LeagueStatistics.Model.LeagueStatistics;
     /// <summary>
     /// Provides console view for league table, results and all League statistics
     /// </summary>
@@ -15,9 +16,40 @@ namespace Codecool.LeagueStatistics.View
             Console.WriteLine($"{team1Name}: {score1} -- {team2Name}: {score2}");
         }
 
-        public static void DisplayFinalResult()
+        public static void DisplayFinalResult(IEnumerable<Team> teams)
         {
+            teams = teams.GetAllTeamsSorted();
+            var longestNamedTeam = teams.GetTeamWithTheLongestName();
+            var maxNameLength = longestNamedTeam.Name.Length;
+            var cellLength = 6;
 
+            var headers = $"|Name{new string(' ', maxNameLength - 4)}|Points| Goals|  Wins| Draws|Losses|";
+            var separator = $"+{new string('-', maxNameLength)}+------+------+------+------+------+";
+
+            Console.WriteLine(separator);
+            Console.WriteLine(headers);
+            foreach (var team in teams)
+            {
+                var goals = team.Players.Sum(player => player.Goals);
+
+                var nameLength = team.Name.Length;
+                var pointsLength = team.CurrentPoints.ToString().Length;
+                var goalsLength = goals.ToString().Length;
+                var winsLength = team.Wins.ToString().Length;
+                var drawsLength = team.Draws.ToString().Length;
+                var lossesLength = team.Losts.ToString().Length;
+
+                var nameCell = $"{team.Name}{new string(' ', maxNameLength - nameLength)}";
+                var pointsCell = $"{new string(' ', cellLength - pointsLength)}{team.CurrentPoints}";
+                var goalsCell = $"{new string(' ', cellLength - goalsLength)}{goals}";
+                var winsCell = $"{new string(' ', cellLength - winsLength)}{team.Wins}";
+                var drawsCell = $"{new string(' ', cellLength - drawsLength)}{team.Draws}";
+                var lossesCell = $"{new string(' ', cellLength - lossesLength)}{team.Losts}";
+
+                Console.WriteLine(separator);
+                Console.WriteLine($"|{nameCell}|{pointsCell}|{goalsCell}|{winsCell}|{drawsCell}|{lossesCell}|");
+            }
+            Console.WriteLine(separator);
         }
     }
 }
